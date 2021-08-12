95d7ca5ddec8640dc78178f95297ae4298015dae
purplehays09 <purplehays527@gmail.com>
2021-08-12 16:41:01 -0600

    I can't figure out the breakout merge issue. I can make it work when I run the file by itself below, but here I am running into issues where it is only keeping the original as the response and not merging the latter. Can you see what I am doing wrong?

--- spec/reunion_spec.rb
+++ spec/reunion_spec.rb
@@ -17,5 +17,50 @@ RSpec.describe Reunion do
 
     expect(reunion.activities[0].name).to eq("Brunch")
   end
+
+  it "can add multiple activities" do
+    reunion.add_activity("Brunch")
+    reunion.add_activity("Drinks")
+
+    reunion.activities[0].add_participant("Maria", 20)
+    reunion.activities[0].add_participant("Luther", 40)
+
+    reunion.activities[1].add_participant("Maria", 10)
+    reunion.activities[1].add_participant("Luther", 40)
+    reunion.activities[1].add_participant("Dave", 95)
+    
+    
+    expect(reunion.activities.length).to eq(2)
+  end
+  
+  it "calculates total cost" do
+    reunion.add_activity("Brunch")
+    reunion.add_activity("Drinks")
+  
+    reunion.activities[0].add_participant("Maria", 20)
+    reunion.activities[0].add_participant("Luther", 40)
+  
+    reunion.activities[1].add_participant("Maria", 10)
+    reunion.activities[1].add_participant("Luther", 40)
+    reunion.activities[1].add_participant("Dave", 95)
+
+    expect(reunion.total_cost).to eq(205)
+  end
+  
+  it "can return everyones bills" do
+    reunion.add_activity("Brunch")
+    reunion.add_activity("Drinks")
+  
+    reunion.activities[0].add_participant("Maria", 20)
+    reunion.activities[0].add_participant("Luther", 40)
+  
+    reunion.activities[1].add_participant("Maria", 10)
+    reunion.activities[1].add_participant("Luther", 40)
+    reunion.activities[1].add_participant("Dave", 100)
+  
+    expect(reunion.breakout.values).to eq([50, 0, -50])
+    
+  end
+  
   
 end
